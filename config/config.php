69be392cc530b4b11210105047c7d3ac6b0fbc48
princeofgiri <ahmadharis1982@gmail.com>
2017-10-22 14:15:49 +0700

    add ridoncloud branding

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocxrob54k37q',
+  'passwordsalt' => 'tUsJqxxVkY6CB96giTL6x1ibGOxim5',
+  'secret' => 'NuCIah9qEfSuQtAn+jSSW3Itli36/tzr/VE8SmcegW3StsWz',
+  'trusted_domains' => 
+  array (
+    0 => '127.0.0.1',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/data',
+  'overwrite.cli.url' => 'http://127.0.0.1',
+  'dbtype' => 'mysql',
+  'version' => '13.0.0.5',
+  'dbname' => 'nextclouddb',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'password',
+  'installed' => true,
+  'theme' => 'ridon',
+);
